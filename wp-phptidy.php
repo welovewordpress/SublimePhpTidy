79f0f1349b1a69862c66ffdf8accf4d113d183b5
Morgan Estes <morgan.estes@gmail.com>
2013-09-13 19:00:40 -0400

    Fixes https://github.com/welovewordpress/SublimePhpTidy/issues/15.
    Add check for single-line DocBlocks and return early if we're in one.

--- wp-phptidy.php
+++ wp-phptidy.php
@@ -1250,9 +1250,22 @@ function fix_docblock_format( &$tokens ) {
 			if ( $line=="" ) continue;
 			if ( $line!="/**" and $line!="*/" ) {
 
-				// Add stars where missing
-				if ( substr( $line, 0, 1 )!="*" ) $line = "* ".$line;
-				elseif ( $line!="*" and substr( $line, 0, 2 )!="* " ) $line = "* ".substr( $line, 1 );
+				// test for single-line comments
+				$docblock_start = '/**';
+				$docblock_end = '*/';
+				if ( substr( $line, 0, 3 ) === $docblock_start and
+					substr_compare($line, $docblock_end, -strlen($docblock_end), strlen($docblock_end)) === 0
+					) {
+					return;
+				} else {
+					// Add stars where missing
+					if ( substr( $line, 0, 1 )!="*" ) {
+					 $line = "* ".$line;
+					}
+					elseif( $line!="*" and substr( $line, 0, 2 )!="* " ) {
+					 $line = "* ".substr( $line, 1 );
+					}
+				}
 
 				// Strip empty lines at the beginning
 				if ( !$comments_started ) {
@@ -2229,7 +2242,7 @@ function add_file_docblock( &$tokens ) {
 
 
 /**
- * Adds funktion DocBlocks where missing
+ * Adds function DocBlocks where missing
  *
  * @param array   $tokens (reference)
  */
